738471d5b8afd0a3ee34a31e92f99286019a0944
miroslav.chakarov <miroslav.chakarov@outlook.com>
2022-04-13 14:04:16 +0300

    ver.1.1

--- src/main.rs
+++ src/main.rs
@@ -8,31 +8,46 @@ use hd44780_hal::HD44780;
 use rppal::{spi::{Spi, Bus, SlaveSelect, Mode, Error},hal::Delay};
 use hx711_spi::Hx711;
 use nb::block;
-//use std::thread;
 
+const one_kg_value: f32 = 130670.0;
+const N: f32 = 30.0;
+const readLoopCount: u8 = 5;
+
+struct AdcData{
+    adcRawVal: f32,
+    adcVal : f32,
+    zeroVal: f32,
+    taraVal: f32,
+    kgVal: f32,
+    previousKgVal: f32,
+}
 
 fn main() -> Result<(), Error> 
 {
+    let mut ADC = AdcData{
+        adcRawVal: 0.0,
+        adcVal : 0.0,
+        zeroVal: 0.0,
+        taraVal: 0.0,
+        kgVal: 0.0,
+        previousKgVal: 0.0,
+    };
+
     //hx711 declarations
     let spi = Spi::new(Bus::Spi0, SlaveSelect::Ss0, 1_000_000, Mode::Mode0)?;
     let mut hx711 = Hx711::new(spi, Delay::new());
-
+    
     //init the screen
 	hx711.reset()?;
 
-    let mut zero_value: f32 = 0.0;
-    let one_kg_value: f32 = 130670.0;
-
-    for i in 0..20 {
+    //calibration (tara)
+    for i in 0..N as i32 {
         let reading = block!(hx711.read()).unwrap() as f32;
         println!("{:>2}: {}", i, reading);
-        zero_value += reading;
+        ADC.zeroVal += reading;
     }
-    zero_value /= 20.0; //tara
-
-    println!("Tara: {}", zero_value);
-
-    let n = 5;
+    ADC.zeroVal /= N; //tara
+    println!("Tara: {}", ADC.zeroVal);
 
     //screen declarations
     let rs = Pin::new(13);
@@ -77,40 +92,63 @@ fn main() -> Result<(), Error>
     lcd.write_str("Customer nr. 4");
 
     lcd.set_cursor_pos(40);
-    lcd.write_str("Calculating...");
+    lcd.write_str("WELCOME!");
     lcd.set_cursor_pos(30);
-    lcd.write_str("0 kg.");
+    lcd.write_str("--- g.");
 
+
+    let mut counter: u8 = 0;
+    let mut flag: bool = false;
     loop {
-        let mut value: f32 = 0.0;
-        let mut tara_val: f32 = 0.0;
-        let mut kg_val: f32 = 0.0;
-        for _ in 0..n {
-            let reading = block!(hx711.read()).unwrap() as f32;
-            value += reading;
-        }
-        value /= n as f32;
-        tara_val = value-zero_value;
-        kg_val = tara_val/one_kg_value;
-        println!(
-            "Read: {} --- Tara val: {} --- kg: {:.2}", 
-            value as i32, 
-            tara_val as i32, 
-            kg_val);
-        thread::sleep_ms(10);
-        lcd.set_cursor_pos(30);
-        lcd.write_str("          ");
-        lcd.set_cursor_pos(30);
-        let s = format!("{:.0} g.", kg_val * 1000.0);
-        lcd.write_str(&s);
+        ADC.previousKgVal = ADC.kgVal;
+        ADC.adcVal = 0.0;
         
-        // for i in 1..6 {
-        //     lcd.set_cursor_pos(0);
-        //     lcd.write_str("Customer nr. ");
-        //     let s = format!("{}", i);
-        //     lcd.write_str(&s);
-        //     thread::sleep(time::Duration::from_millis(1000));
-        // }
+        for _ in 0..readLoopCount {
+            ADC.adcRawVal = block!(hx711.read()).unwrap() as f32;
+            ADC.adcVal += ADC.adcRawVal;
+        }
+        ADC.adcVal /= readLoopCount as f32;
+        ADC.taraVal = ADC.adcVal-ADC.zeroVal;
+        ADC.kgVal = ADC.taraVal/one_kg_value;
+        println!(
+            "Read: {} --- Tara val: {} --- kg: {:.3}", 
+            ADC.adcVal as i32, 
+            ADC.taraVal as i32, 
+            ADC.kgVal);
+        //lcd.set_cursor_pos(30);
+       // let s = format!("{:.0} g.   ", ADC.kgVal * 1000.0);
+       // lcd.write_str(&s);
+        
+        if (ADC.kgVal - ADC.previousKgVal) > 0.002 {
+            println!("--- START LISTENING --- {}", ADC.kgVal - ADC.previousKgVal);
+            lcd.set_cursor_pos(40);
+            lcd.write_str("Calculating...");
+        }
+
+        if (ADC.previousKgVal - ADC.kgVal) > 0.002 {
+            println!("---STOP LISTENING--- To be added: {:.3}", ADC.kgVal);
+            lcd.set_cursor_pos(40);
+            lcd.write_str("Almost there...");
+
+
+            flag = true;
+            counter = 0;
+        }
+        
+        if flag == true {
+            counter += 1;
+            lcd.set_cursor_pos(30);
+            let s = format!("{:.0} g.   ", (ADC.kgVal * 1000.0).abs());
+            lcd.write_str(&s);
+            if counter == 5{
+                println!("{:.3} added to customer", ADC.kgVal);
+                
+                lcd.set_cursor_pos(40);
+                lcd.write_str("Green salad:   ");
+                flag = false;
+                counter = 0;
+            }
+        }
     }
 }
 
@@ -130,4 +168,4 @@ fn main() -> Result<(), Error>
 //         lcd.write_str("Calculating...  ");
 //         thread::sleep(time::Duration::from_millis(200));
 //     }
-//}
+// }
